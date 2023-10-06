e209c5309d2cf5ae4acbbcc5cb1e409264c24292
DanielSiilva <daniellsousa0614@gmail.com>
2023-10-06 19:05:03 -0300

    app config

--- src/app.js
+++ src/app.js
@@ -2,10 +2,7 @@ import express from 'express';
 import categoriesRouter from './routes/categories.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use('/api/categories', categoriesRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+export default app;
