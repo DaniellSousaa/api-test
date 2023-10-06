0cfeee4fe55f1b158ea4d248a1d9f3ecac4223e3
DanielSiilva <daniellsousa0614@gmail.com>
2023-10-06 20:20:32 -0300

    feat: add cores

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import categoriesRouter from './routes/categories.js';
 
 const app = express();
 
+// Use o middleware CORS
+app.use(cors());
+
 app.use('/api/categories', categoriesRouter);
 
+// Exporte o app para uso com Vercel
 export default app;
